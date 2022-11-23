e0d5c505336542d0826ff89482a4a339180dee4b
Riddhish <riddhish.lichade@gmail.com>
2022-11-23 16:08:05 +0530

    completed getJavaFiles function

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import re
 
 
 def checkImportStatement(line):
+    #Riddhish
     """Checks if the given line contains a import statement and returns the java file if found else returns False"""
     expression = re.compile(r"^import (.+)\s")  # regex for import statement
     javaFile = re.search(expression, line)
@@ -15,6 +16,7 @@ def checkImportStatement(line):
 
 
 def checkImportedFile(file):
+    #Aryan
     """Checks if the imported file is a java library file or user-defined
         Returns True if the file is user-defined else returns False"""
     # os.path.exists checks if the file imported is user-defined or standard library
@@ -32,13 +34,21 @@ def traverseJavaFile(file):
 def outputData(importedFiles):
     """Receives a dictionary containing filename as key and list of user-defined imported java files as values
         Writes the data into a csv file"""
-    pass
+    #importedFiles = {file1:[file2,file3,file4],file2:[file3,file4]}
+    with open("java_dependencies.csv","w+") as csvFile:
+        head=list(importedFiles.keys())
+        head.insert(0,"Dependency")
+        csvWriter = csv.writer(csvFile)
+        csvWriter.writerow(head)
+
 
 
 def getJavaFiles():
+    #Riddhish
     """This function returns list of names of all the java files present in the 'java_files' directory"""
     javaFiles=[]
-    allFiles = os.listdir('java_files')
+    pathToJavaFiles = 'java_files'
+    allFiles = os.listdir(pathToJavaFiles)
     for i in allFiles:
         if i.endswith(".java"):
             javaFiles.append(i)
